c965575d8f4ce8ca22a7a1c77686ed45ac356223
razz hdz <razz@debian-BULLSEYE-live-builder-AMD64>
2022-02-15 19:26:08 -0600

    Proyecto creado

---
+++ ??.py
@@ -0,0 +1,28 @@
+
+
+respultado = int(input("PIDE UN DATO: "))
+
+
+distancia_a_alfa_centauri = "String"
+
+print(type(distancia_a_alfa_centauri))
+
+distancia_a_alfa_centauri = 5
+
+print(type(distancia_a_alfa_centauri))
+
+distancia_a_alfa_centauri = 5.1
+
+print(type(distancia_a_alfa_centauri))
+
+x = 5
+x*=2
+print(x)
+
+from datetime import date
+
+fecha_actual = date.today()
+
+print(type(fecha_actual))
+
+print("LA FECHA DE HOY ES " + str(fecha_actual))
